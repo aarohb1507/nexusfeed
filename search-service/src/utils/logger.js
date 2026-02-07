9afa9b5471acbd39539286e8626db9d1f0d4121b
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2026-02-07 06:18:57 +0530

    Fix search-service logger name

--- search-service/src/utils/logger.js
+++ search-service/src/utils/logger.js
@@ -2,13 +2,13 @@ const winston = require('winston');
 
 const logger = winston.createLogger({
     level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-    format : winston.format.combine(
+    format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.splat(),
         winston.format.errors({ stack: true }),
         winston.format.json()
     ),
-    defaultMeta: { service: 'media-service' },   
+    defaultMeta: { service: 'search-service' },
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
